2340d3d23f3d9dc0b069bbc5668922ff33e75eed
gopalareddy329 <pmallireddy329@gmail.com>
2024-12-10 20:34:51 +0530

    ui

---
+++ client/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import ImageSection from './Image/ImageSection';
+
+
+const HomePage = () => {
+  
+
+  
+
+    return(
+    
+  <div className="min-h-full h-full  flex flex-col mt-5 ">
+
+    <div className='h-full w-full'>
+          <ImageSection/>
+    </div>
+    
+   
+  </div>
+    )
+};
+
+export default HomePage;
